ea2109689e16d2d52c2617b347bdb6eee0b0d2e4
UnRealDinnerbone <unrealdinnerbone@gmail.com>
2020-11-02 15:11:46 -0600

    Fixed

--- src/main/java/com/unrealdinnerbone/javd/data/DataEvent.java
+++ src/main/java/com/unrealdinnerbone/javd/data/DataEvent.java
@@ -1,2 +1,117 @@
-package com.unrealdinnerbone.javd.data;public class DataEvent {
+package com.unrealdinnerbone.javd.data;
+
+import com.mojang.datafixers.util.Pair;
+import com.unrealdinnerbone.javd.JAVD;
+import com.unrealdinnerbone.javd.JAVDRegistry;
+import net.minecraft.block.Block;
+import net.minecraft.data.*;
+import net.minecraft.data.loot.BlockLootTables;
+import net.minecraft.item.Items;
+import net.minecraft.loot.LootParameterSet;
+import net.minecraft.loot.LootParameterSets;
+import net.minecraft.loot.LootTable;
+import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.client.model.generators.BlockStateProvider;
+import net.minecraftforge.common.Tags;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.common.data.ForgeLootTableProvider;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraftforge.fml.event.lifecycle.GatherDataEvent;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+
+public class DataEvent {
+
+    public static void onData(GatherDataEvent event) {
+        event.getGenerator().addProvider(new Recipe(event.getGenerator()));
+        event.getGenerator().addProvider(new BlockState(event.getGenerator(), event.getExistingFileHelper()));
+        event.getGenerator().addProvider(new Item(event.getGenerator(), event.getExistingFileHelper()));
+        event.getGenerator().addProvider(new LootTable(event.getGenerator()));
+    }
+
+    public static class LootTable extends ForgeLootTableProvider {
+
+        public LootTable(DataGenerator gen) {
+            super(gen);
+        }
+
+        @Override
+        protected List<Pair<Supplier<Consumer<BiConsumer<ResourceLocation, net.minecraft.loot.LootTable.Builder>>>, LootParameterSet>> getTables() {
+            return Collections.singletonList(Pair.of(BlockLootTable::new, LootParameterSets.BLOCK));
+        }
+
+        public static class BlockLootTable extends BlockLootTables {
+
+            @Override
+            public void addTables() {
+                registerDropSelfLootTable(JAVDRegistry.PORTAL_BLOCK.get());
+            }
+
+            protected Iterable<Block> getKnownBlocks() {
+                List<Block> blocks = new ArrayList<>();
+                blocks.add(JAVDRegistry.PORTAL_BLOCK.get());
+                return blocks;
+            }
+
+
+        }
+    }
+
+    public static class Item extends net.minecraftforge.client.model.generators.ItemModelProvider {
+
+        public Item(DataGenerator generator, ExistingFileHelper existingFileHelper) {
+            super(generator, JAVD.MOD_ID, existingFileHelper);
+        }
+
+        @Override
+        protected void registerModels() {
+            itemGenerated(JAVDRegistry.PORTAL_BLOCK_ITEM.get(), new ResourceLocation(JAVD.MOD_ID, "block/portal_block"));
+        }
+
+        public void itemGenerated(net.minecraft.item.Item item, ResourceLocation texture) {
+            getBuilder(item.getRegistryName().getPath()).parent(getExistingFile(mcLoc("item/generated")))
+                    .texture("layer0", texture);
+        }
+
+    }
+
+    public static class BlockState extends BlockStateProvider {
+
+        public BlockState(DataGenerator gen, ExistingFileHelper exFileHelper) {
+            super(gen, JAVD.MOD_ID, exFileHelper);
+        }
+
+        @Override
+        protected void registerStatesAndModels() {
+            simpleBlock(JAVDRegistry.PORTAL_BLOCK.get());
+        }
+    }
+
+    public static class Recipe extends RecipeProvider {
+
+        public Recipe(DataGenerator generatorIn) {
+            super(generatorIn);
+        }
+
+        @Override
+        protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
+            ShapedRecipeBuilder.shapedRecipe(JAVDRegistry.PORTAL_BLOCK_ITEM::get)
+                    .patternLine("OOO")
+                    .patternLine("OEO")
+                    .patternLine("OOO")
+                    .key('O', Tags.Items.OBSIDIAN)
+                    .key('E', Items.ENDER_EYE)
+                    .addCriterion("has_eye", hasItem(Items.ENDER_EYE))
+                    .addCriterion("has_obsidian", hasItem(Items.OBSIDIAN))
+                    .build(consumer);
+        }
+    }
 }
